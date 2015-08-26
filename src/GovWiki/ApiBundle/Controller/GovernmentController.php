d2414292d38b18b834226b19f95267f27d28e0b4
Sergey Voronov <sergey.voronov@sibers.com>
2015-08-26 18:35:02 +0600

    Add api controller

---
+++ src/GovWiki/ApiBundle/Controller/GovernmentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace GovWiki\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use GovWiki\DbBundle\Entity\Government;
+use GovWiki\DbBundle\Entity\ElectedOfficial;
+
+/**
+ * GovernmentController
+ */
+class GovernmentController extends Controller
+{
+    /**
+     * @Route("/{slug}/", methods="GET")
+     */
+    public function showAction(Government $government)
+    {
+        $normalizer  = new ObjectNormalizer();
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getName();
+        });
+
+        $serializer  = new Serializer([$normalizer], [new JsonEncoder()]);
+
+        $response = new Response();
+        $response->setContent($serializer->serialize($government, 'json'));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
