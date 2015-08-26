ebe7c7595e9d4ea4437268563b1327d213c4a276
Sergey Voronov <sergey.voronov@sibers.com>
2015-08-26 14:28:57 +0600

    Create API bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new GovWiki\DbBundle\GovWikiDbBundle(),
+            new GovWiki\ApiBundle\GovWikiApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/GovWiki/ApiBundle/GovWikiApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GovWiki\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GovWikiApiBundle extends Bundle
+{
+}
