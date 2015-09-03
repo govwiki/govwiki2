aaee0f3b6bc75759812e404028b9d05ea55ace50
Sergey Voronov <sergey.voronov@sibers.com>
2015-09-03 15:22:27 +0600

    Update API controller for JMS

--- src/GovWiki/ApiBundle/Controller/GovernmentController.php
+++ src/GovWiki/ApiBundle/Controller/GovernmentController.php
@@ -4,14 +4,9 @@ namespace GovWiki\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use GovWiki\DbBundle\Entity\Government;
-use GovWiki\DbBundle\Entity\ElectedOfficial;
+use JMS\Serializer\SerializationContext;
 
 /**
  * GovernmentController
@@ -26,15 +21,10 @@ class GovernmentController extends Controller
      */
     public function showAction(Government $government)
     {
-        $normalizer  = new ObjectNormalizer();
-        $normalizer->setCircularReferenceHandler(function ($object) {
-            return;
-        });
-
-        $serializer  = new Serializer([$normalizer], [new JsonEncoder()]);
+        $serializer = $this->get('jms_serializer');
 
         $response = new Response();
-        $response->setContent($serializer->serialize($government, 'json'));
+        $response->setContent($serializer->serialize($government, 'json', SerializationContext::create()->enableMaxDepthChecks()));
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
