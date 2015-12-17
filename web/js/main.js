e46ef9d2feaccce4f8498129a64385f7f0c16475
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2015-12-17 20:26:56 +0600

    fix assetes

--- web/js/main.js
+++ web/js/main.js
@@ -13,158 +13,332 @@ $('body').on('submit', '#ajax-login-form', function(event) {
     });
 });
 
+/**
+ * Typeahead search
+ */
+
 $(function() {
 
-    // Popover window (Rank over all altTypes)
-    $('.statistics').popover({
-        placement: 'bottom',
-        selector: '.rank',
-        animation: true,
-        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-title-custom"><h3 class="popover-title"></h3></div><div class="popover-content"></div></div>'
-    });
+    var substringMatcher = function(strs) {
+        return function findMatches(q, cb) {
+            var matches, substringRegex;
 
+            // an array that will be populated with substring matches
+            matches = [];
 
-    $('.governmentController').on('click', function(e) {
-        var $element, additionalRowsTpl, currentPage, fieldName, fieldNameInCamelCase, formatData, loadNewRows, loading, mask, popoverContent, popoverNameOrder, popoverOrder, popoverTpl, preloader, previousScrollTop;
-        $element = $(e.target);
-        popoverContent = $element.parent().find('.popover-content');
-        fieldName = $element.attr('data-field');
-        mask = $element.attr('data-mask');
-        popoverTpl = $('#rankPopover').html();
-        additionalRowsTpl = $('#additionalRows').html();
-        preloader = popoverContent.find('loader');
-        previousScrollTop = 0;
-        currentPage = 0;
-        loading = false;
-        popoverOrder = null;
-        popoverNameOrder = null;
-        if (!$element.closest('.popover')[0]) {
-            $('.rank').not(e.target).popover('destroy');
-        }
-        formatData = function(data) {
-            if (mask) {
-                return data.data.forEach(function(rank) {
-                    return rank.amount = numeral(rank.amount).format(mask);
-                });
-            }
-        };
-        loadNewRows = function() {
-            var fieldNameInCamelCase, table;
-            loading = true;
-            preloader.show();
-            table = popoverContent.find('table tbody');
-            table.html('');
-            currentPage = 0;
-            previousScrollTop = 0;
-            fieldNameInCamelCase = fieldName.replace(/_([a-z0-9])/g, function(g) {
-                return g[1].toUpperCase();
-            });
-            return $.ajax({
-                url: '/api/v1/government' + window.location.pathname + '/get_ranks',
-                dataType: 'json',
-                data: {
-                    page: currentPage,
-                    order: popoverOrder,
-                    name_order: popoverNameOrder,
-                    field_name: fieldNameInCamelCase
-                },
-                success: function(data) {
-                    var compiledTemplate;
-                    formatData(data);
-                    compiledTemplate = Handlebars.compile(additionalRowsTpl);
-                    table.html(compiledTemplate(data));
-                    loading = false;
-                    return preloader.hide();
+            // regex used to determine if a string contains the substring `q`
+            substrRegex = new RegExp(q, 'i');
+
+            // iterate through the pool of strings and for any string that
+            // contains the substring `q`, add it to the `matches` array
+            $.each(strs, function(i, str) {
+                if (substrRegex.test(str.gov_name)) {
+                    matches.push(str);
                 }
             });
+
+            cb(matches);
         };
-        popoverContent.on('click', 'th', function(e) {
-            var $column;
-            $column = $(e.target).hasClass('sortable') ? $(e.target) : $(e.target).closest('th');;
-            if ($column.hasClass('sortable')) {
-                if ($column.hasClass('desc')) {
-                    if ($column.attr('data-sort-type') === 'name_order') {
-                        popoverNameOrder = '';
-                    } else {
-                        popoverOrder = '';
-                    }
-                    loadNewRows();
-                    $column.removeClass('desc').removeClass('asc');
-                    return $column.find('i').removeClass('icon__bottom').removeClass('icon__top');
-                } else if ($column.hasClass('asc')) {
-                    if ($column.attr('data-sort-type') === 'name_order') {
-                        popoverNameOrder = 'desc';
-                    } else {
-                        popoverOrder = 'desc';
-                    }
-                    loadNewRows();
-                    $column.removeClass('asc').addClass('desc');
-                    return $column.find('i').removeClass('icon__top').addClass('icon__bottom');
-                } else {
-                    if ($column.attr('data-sort-type') === 'name_order') {
-                        popoverNameOrder = 'asc';
-                    } else {
-                        popoverOrder = 'asc';
-                    }
-                    loadNewRows();
-                    $column.addClass('asc');
-                    return $column.find('i').addClass('icon__top');
-                }
+    };
+
+    $.get('/legacy/data/h_types_ca_2.json', function (data){
+
+        var searchValue = '';
+
+        // Init typeahead
+        var $typeahead = $('.typeahead').typeahead({
+            hint: true,
+            minLength: 1
+        }, {
+            name: 'countries',
+            source: substringMatcher(data.record),
+            templates: {
+                empty: '<div class="tt-suggestion">Not found. Please retype your query </div>',
+                suggestion: Handlebars.compile('<div class="sugg-box">'+
+                    '<div class="sugg-state">{{state}}</div>' +
+                    '<div class="sugg-name">{{gov_name}}</div>' +
+                    '<div class="sugg-type">{{gov_type}}</div>' +
+                    '</div>')
+            },
+            updater: function (item) {
+                alert(item);
             }
         });
-        if (fieldName) {
-            fieldNameInCamelCase = fieldName.replace(/_([a-z0-9])/g, function(g) {
-                return g[1].toUpperCase();
+
+        // Pressed mouse or enter button
+        $typeahead.bind("typeahead:selected", function(obj, selectedItemData) {
+            $typeahead.typeahead('val', selectedItemData.gov_name);
+            window.location.pathname = [selectedItemData.altTypeSlug, selectedItemData.slug].join('/');
+        });
+
+        // Move cursor via arrows keys
+        $typeahead.bind("typeahead:cursorchange", function(obj) {
+            $typeahead.typeahead('val', searchValue);
+        });
+
+        // Store search value on typing
+        $typeahead.keyup(function(event) {
+            searchValue = $(event.target).val();
+        });
+
+    });
+
+});
+$(function() {
+
+    /**
+     * window.gw.map = {
+     *  id: Number
+     *  vizURL: String
+     *  centerLatitude: Number
+     *  centerLongitude: Number
+     *  zoom: Number
+     * }
+     */
+
+    window.gw.map = JSON.parse(window.gw.map);
+
+    cartodb.createVis('map', 'https://dam-robert.cartodb.com/api/v2/viz/935d0bdc-9d7a-11e5-a98e-0e787de82d45/viz.json', {
+            scrollwheel: true,
+            shareable: false,
+            title: false,
+            description: false,
+            search: false,
+            tiles_loader: true,
+            center_lat: 37.3,
+            center_lon: -119.3,
+            zoom: 5
+        })
+        .done(function(vis, layers) {
+
+            var map = vis.getNativeMap();
+            // map.setZoom(3);
+            // map.panTo([50.5, 30.5]);
+
+            // layer 0 is the base layer, layer 1 is cartodb layer
+            var layer = layers[1];
+
+            var countyLayer = layer.getSubLayer(0),
+                cityLayer = layer.getSubLayer(1),
+                schoolLayer = layer.getSubLayer(2),
+                specialLayer = layer.getSubLayer(3);
+
+            var sublayers = [];
+            sublayers.push(countyLayer);
+            sublayers.push(cityLayer);
+            sublayers.push(schoolLayer);
+            sublayers.push(specialLayer);
+
+            countyLayer.set({ 'interactivity': ['cartodb_id', 'slug', 'geometry'] }); // alias to template
+            cityLayer.set({ 'interactivity': ['cartodb_id', 'slug'] }); // alias to template
+            schoolLayer.set({ 'interactivity': ['cartodb_id', 'slug'] }); // alias to template
+            specialLayer.set({ 'interactivity': ['cartodb_id', 'slug'] }); // alias to template
+
+            /**
+             * Passing data and render layers
+             */
+            //var prom = countyLayer.setSQL('SELECT  *, ST_AsGeoJSON(ST_Simplify(the_geom,.01)) AS geometry FROM ' + window.gw.environment + '_county');
+            //cityLayer.setSQL("SELECT * FROM " + window.gw.environment + " WHERE ");
+            //schoolLayer.setSQL("SELECT * FROM school_district_ca");
+            //specialLayer.setSQL("SELECT * FROM special_discrict_ca");
+
+            var prom = countyLayer.setSQL('SELECT  *, ST_AsGeoJSON(ST_Simplify(the_geom,.01)) AS geometry FROM county_ca');
+            cityLayer.setSQL("SELECT * FROM city_ca");
+            schoolLayer.setSQL("SELECT * FROM school_district_ca");
+            specialLayer.setSQL("SELECT * FROM special_discrict_ca");
+
+            var $objectsPane = $('.leaflet-objects-pane');
+            var $tilePane = $('.leaflet-tile-pane');
+
+            $objectsPane.appendTo($tilePane);
+            $objectsPane.css({"z-index":"100"});
+
+            /*
+             * Tooltip vindow
+             * Tooltip work with 3.11-13 version, 3.15 is buggy
+             */
+
+            cdb.geo.ui.Tooltip.prototype.getLayerIndex = function () {
+                return this.options.layer._position;
+            };
+
+            var countyTooltip = new cdb.geo.ui.Tooltip({
+                layer: countyLayer,
+                template: '<div class="cartodb-tooltip-content-wrapper"> <div class="cartodb-tooltip-content"><p>{{slug}}</p></div></div>',
+                width: 200,
+                position: 'bottom|right'
             });
-            $.ajax({
-                url: '/api/v1/government' + window.location.pathname + '/get_ranks',
-                dataType: 'json',
-                data: {
-                    field_name: fieldNameInCamelCase
-                },
-                success: function(data) {
-                    var compiledTemplate;
-                    formatData(data);
-                    compiledTemplate = Handlebars.compile(popoverTpl);
-                    return popoverContent.html(compiledTemplate(data));
-                }
+
+            var cityTooltip = new cdb.geo.ui.Tooltip({
+                layer: cityLayer,
+                template: '<div class="cartodb-tooltip-content-wrapper"> <div class="cartodb-tooltip-content"><p>{{slug}}</p></div></div>',
+                width: 200,
+                position: 'bottom|right'
             });
-        }
-        return popoverContent.scroll(function() {
-            var currentScrollTop;
-            currentScrollTop = popoverContent.scrollTop();
-            if (previousScrollTop < currentScrollTop && currentScrollTop > 0.5 * popoverContent[0].scrollHeight) {
-                previousScrollTop = currentScrollTop;
-                if (loading === false) {
-                    loading = true;
-                    preloader.show();
-                    return $.ajax({
-                        url: '/api/v1/government' + window.location.pathname + '/get_ranks',
-                        dataType: 'json',
-                        data: {
-                            page: ++currentPage,
-                            order: popoverOrder,
-                            name_order: popoverNameOrder,
-                            field_name: fieldNameInCamelCase
-                        },
-                        success: function(data) {
-                            var compiledTemplate;
-                            formatData(data);
-                            loading = false;
-                            preloader.hide();
-                            compiledTemplate = Handlebars.compile(additionalRowsTpl);
-                            popoverContent.find('table tbody')[0].innerHTML += compiledTemplate(data);
-                            return console.log(data);
+//
+            var schoolTooltip = new cdb.geo.ui.Tooltip({
+                layer: schoolLayer,
+                template: '<div class="cartodb-tooltip-content-wrapper"> <div class="cartodb-tooltip-content"><p>{{slug}}</p></div></div>',
+                width: 200,
+                position: 'bottom|right'
+            });
+
+            var specialTooltip = new cdb.geo.ui.Tooltip({
+                layer: specialLayer,
+                template: '<div class="cartodb-tooltip-content-wrapper"> <div class="cartodb-tooltip-content"><p>{{slug}}</p></div></div>',
+                width: 200,
+                position: 'bottom|right'
+            });
+
+            var tooltips = [countyTooltip, cityTooltip, schoolTooltip, specialTooltip];
+
+            tooltips.forEach(function (tooltip) {
+                $('#map_wrap').append(tooltip.render().el);
+            });
+
+            var hovers = [];
+
+            /**
+             * Set handlers on sublayers
+             */
+            sublayers.forEach(function(layer) {
+
+                layer.setInteraction(true);
+
+                layer.bind('mouseover', function(e, latlon, pxPos, data, layerIndex) {
+
+                    // TODO: Must be deleted, when data will be replaced, now it's hardcoded
+                    data.slug = data.slug.replace(/_/g, ' ');
+
+                    hovers[layerIndex] = 1;
+
+                    if(_.any(hovers)) {
+
+                        $('.cartodb-map-wrapper').css('cursor', 'pointer');
+
+                        if (layerIndex == countyLayer._position) {
+                            drawAppropriatePolygon(data);
+                        } else {
+                            removeAllHoverShapes();
                         }
-                    });
+
+                        // Open current tooltip, close another
+                        tooltips.forEach(function(tooltip){
+                            if (tooltip.getLayerIndex() == layerIndex) {
+                                tooltip.enable();
+                            } else {
+                                tooltip.disable();
+                            }
+                        })
+
+                    }
+
+                });
+
+                layer.bind('mouseout', function(layerIndex) {
+                    hovers[layerIndex] = 0;
+                    if(!_.any(hovers)) {
+                        $('.cartodb-map-wrapper').css('cursor', 'auto');
+
+                        removeAllHoverShapes();
+
+                        // Close all tooltips, if cursor outside of layers
+                        tooltips.forEach(function(tooltip){
+                            if (tooltip.getLayerIndex() == layerIndex) {
+                                tooltip.disable();
+                            }
+                        })
+
+                    }
+                });
+
+                layer.on('featureClick', function (event, latlng, pos, data, layerIndex) {
+
+
+
+                    /**
+                     * TODO: hardcoded, must be replaced on multi envirenment
+                     * @type {string}
+                     */
+                    var altTypeSlug = '';
+                    switch (layerIndex) {
+                        case 0:
+                            altTypeSlug = 'County';
+                            break;
+
+                        case 1:
+                            altTypeSlug = 'City';
+                            break;
+
+                        case 2:
+                            altTypeSlug = 'School_District';
+                            break;
+
+                        case 3:
+                            altTypeSlug = 'Special_District';
+                            break;
+                    }
+
+                    var governmentSlug = data.slug.replace(/ /g, '_');
+
+                    window.location.pathname = altTypeSlug + '/' + governmentSlug;
+                });
+
+            });
+
+            // Toggle layers
+            $('.legend-item').click(function() {
+                $(this).toggleClass('selected');
+                LayerActions[$(this).attr('id')]();
+            });
+
+            var LayerActions = {
+                counties: function(){
+                    countyLayer.toggle();
+                    return true;
+                },
+                cities: function(){
+                    cityLayer.toggle();
+                    return true;
+                },
+                school: function(){
+                    schoolLayer.toggle();
+                    return true;
+                },
+                special: function(){
+                    specialLayer.toggle();
+                    return true;
                 }
+            };
+
+            // Polygon variables and functions
+            var polygon = {};
+            // What should our polygon hover style look like?
+            var polygon_style = {
+                color: "#808080",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: .45,
+                fillColor: "#00FF00",
+                clickable: false
+            };
+
+            function drawAppropriatePolygon(data){
+                removeAllHoverShapes();
+                polygon = new L.GeoJSON(JSON.parse(data.geometry), {
+                    style: polygon_style
+                });
+                map.addLayer(polygon);
+                polygon.cartodb_id = data.cartodb_id;
             }
+            function removeAllHoverShapes(){
+                map.removeLayer(polygon);
+                polygon.cartodb_id = null;
+            }
+
+        })
+        .error(function(err) {
+            console.log(err);
         });
-    });
-
-
-
-
-
 
 });
