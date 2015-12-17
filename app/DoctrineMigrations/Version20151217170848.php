0c2017ab34134047a321965c6f880dac64bf32fc
Sergey Voronov <sergey.voronov@sibers.com>
2015-12-17 17:25:01 +0600

    Add alt_type to max_ranks

---
+++ app/DoctrineMigrations/Version20151217170848.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * 20151217170848
+ */
+class Version20151217170848 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE max_ranks ADD alt_type VARCHAR(20) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE max_ranks DROP alt_type');
+    }
+}
