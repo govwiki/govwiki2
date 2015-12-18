8431a31034e13b914473efdf09d083c0f0f9460c
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2015-12-18 19:21:48 +0600

    add migration

---
+++ app/DoctrineMigrations/Version20151218145914.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151218145914 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE environments ADD style LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', DROP header, DROP footer');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE environments ADD header LONGTEXT NOT NULL, ADD footer LONGTEXT NOT NULL, DROP style');
+    }
+}
