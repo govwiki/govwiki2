be8c7a1a1653e14492a4eb389087858f454532d1
RD <dampilon.robert@gmail.com>
2015-12-18 19:32:04 +0600

    Refactor government page (restore ranks popover)

---
+++ src/GovWiki/FrontendBundle/Resources/public/js/government/index.js
@@ -0,0 +1,5 @@
+$(function() {
+
+    var rankPopover = new RankPopover();
+
+});
--- src/GovWiki/FrontendBundle/Resources/public/js/government/rank-popover.js
+++ src/GovWiki/FrontendBundle/Resources/public/js/government/rank-popover.js
@@ -1,19 +1,4 @@
 
-$('body').on('submit', '#ajax-login-form', function(event) {
-    event.preventDefault();
-    $form = $(this);
-    $form.parent().find('.alert').remove();
-    console.log($form.serialize());
-    $.post('/login_check', $form.serialize(), function(data) {
-        if (data.error) {
-            $form.parent().prepend('<div class="alert alert-warning">' + data.error + '</div>');
-        } else {
-            location.reload();
-        }
-    });
-});
-
-
 /**
  * @param {Object} options
  * @param {String} options.selector
@@ -315,8 +300,3 @@ RankPopover.prototype.formatData = function formatData (data) {
         return buffer;
     },"useData":true});
 })();
-$(function() {
-
-    var rankPopover = new RankPopover();
-
-});
