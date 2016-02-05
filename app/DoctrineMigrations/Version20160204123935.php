a614cbb11a0bd7deec70b52dee4904b34116ee31
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-02-05 22:36:25 +0600

    fix migration

--- app/DoctrineMigrations/Version20160204123935.php
+++ app/DoctrineMigrations/Version20160204123935.php
@@ -18,7 +18,7 @@ class Version20160204123935 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE governments DROP annual_salary_mayor, DROP years_mayor_in_office, CHANGE mayor_name secondary_logo_path VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE governments ADD mayor_name secondary_logo_path VARCHAR(255) DEFAULT NULL');
         $this->addSql('UPDATE governments SET secondary_logo_path = secondary_logo_url');
         $this->addSql('UPDATE governments SET secondary_logo_url = NULL');
     }
@@ -33,6 +33,6 @@ class Version20160204123935 extends AbstractMigration
 
         $this->addSql('UPDATE governments SET secondary_logo_url = secondary_logo_path');
         $this->addSql('UPDATE governments SET secondary_logo_path = NULL');
-        $this->addSql('ALTER TABLE governments ADD annual_salary_mayor DOUBLE PRECISION DEFAULT NULL, ADD years_mayor_in_office INT DEFAULT NULL, CHANGE secondary_logo_path mayor_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE DROP secondary_logo_path mayor_name VARCHAR(255) DEFAULT NULL');
     }
 }
