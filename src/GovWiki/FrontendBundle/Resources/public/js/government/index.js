06fe6be16f3174c6559df6ab73fdcbda77ec3e5d
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-02-10 16:08:32 +0600

    open proper tab

--- src/GovWiki/FrontendBundle/Resources/public/js/government/index.js
+++ src/GovWiki/FrontendBundle/Resources/public/js/government/index.js
@@ -354,6 +354,13 @@ $(function() {
     $('#fin-stmt-year').change(function() {
         var $this = $(this);
         $this.closest('form').submit();
+        window.localStorage.setItem('tab', 'Financial_Statements');
     });
 
+
+    var tab = window.localStorage.getItem('tab');
+    if (tab) {
+        window.localStorage.removeItem('tab');
+        $('.nav-pills a[href="#' + tab + '"]').tab('show');
+    }
 });
