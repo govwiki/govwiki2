c76a8d750051c65826af4d62097a941fe019e5b1
Bator Rybdylov <bator.rybdylov@sibers.com>
2016-02-29 14:19:31 +0600

    Migrations files.

---
+++ app/DoctrineMigrations/Version20160205220202.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160205220202 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE environments ADD bottom_text LONGTEXT DEFAULT NULL, ADD show_bottom_text TINYINT(1) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE environments DROP bottom_text, DROP show_bottom_text');
+    }
+}
--- app/DoctrineMigrations/Version20160229140334.php
+++ app/DoctrineMigrations/Version20160229140334.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160220152106 extends AbstractMigration
+class Version20160229140334 extends AbstractMigration
 {
     /**
      * @param Schema $schema
