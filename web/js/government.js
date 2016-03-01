ffdd1348099734e5d54caca86b83a0d8eed78e19
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-03-01 21:19:49 +0600

    rank popover fix

--- web/js/government.js
+++ web/js/government.js
@@ -426,11 +426,231 @@ webpackJsonp([1],[
 	 * Initialization
 	 */
 	function init() {
+	
+	    hideChartGroup('pie-charts', false);
+	    hideChartGroup('tree-chart', true);
+	    hideChartGroup('tree-charts', true);
 	    handler_switchChart();
+	    //financialStatements_compare().init();
 	    financialStatements_revenue();
 	    financialStatements_expenditures();
 	    financialStatementsTree_expenditures();
 	    financialStatementsTree_revenues();
+	
+	}
+	
+	function financialStatements_compare() {
+	
+	    //var compareGovernments = {
+	    //    data: null,
+	    //    init: function() {
+	    //        var obj = this;
+	    //
+	    //        // search goverments
+	    //        $('.municipality-compare__goverments').keyup(function(event) {
+	    //            if (event.keyCode >= 9 && event.keyCode <= 46) {
+	    //            } else {
+	    //                var el = $(this);
+	    //                el.parent().find('.municipality-search').html('');
+	    //                if (el.val().length > 2) {
+	    //                    $.ajax({
+	    //                        url: '{{ govwiki_path('govwiki_api_v1_government_search') }}',
+	    //                        type: 'GET',
+	    //                        data: 'search=' + el.val(),
+	    //                        success: function (data) {
+	    //                            if (data.length > 0) {
+	    //                                for (i = 0; i < data.length; i++) {
+	    //                                    el.parent().find('.municipality-search').append('<div onclick="compareGovernments.setYear(' + data[i].id + ', \'' + el.attr('id') + '\', \'' + data[i].name + '\')">' + data[i].state + ' ' + data[i].name + '</div>');
+	    //                                }
+	    //                            }
+	    //                        }
+	    //                    });
+	    //                }
+	    //            }
+	    //        });
+	    //
+	    //        // submit form
+	    //        $('.municipality-compare').submit(function(e) {
+	    //            e.preventDefault();
+	    //            obj.submitForm($(this));
+	    //        });
+	    //
+	    //        // check years for get category
+	    //        $('.municipality-year-select').change(function() {
+	    //            if ($('#first-year').val() != '' && $('#second-year').val() != '') {
+	    //                obj.getCategories();
+	    //            } else {
+	    //                $('#municipality-categories').html('<option value="">All categories</option>');
+	    //            }
+	    //        });
+	    //
+	    //    },
+	    //    setYear: function(govId, elId, name) {
+	    //        var el = $('#'+elId);
+	    //        var yearEl = el.parent().next().find('select');
+	    //
+	    //        // get goverments years
+	    //        $.ajax({
+	    //            url: location.href,
+	    //            type: 'POST',
+	    //            data: 'yearByGovId=' + govId,
+	    //            success: function (data) {
+	    //                if (data.length > 0) {
+	    //                    yearEl.html('<option value="">Year</option>');
+	    //                    for (i=0;i<data.length;i++) {
+	    //                        el.parent().next().find('select').append('<option value="'+data[i].id+'">'+data[i].year+'</option>');
+	    //                    }
+	    //
+	    //                    el.val(name);
+	    //                    el.attr('data-id', govId);
+	    //                    el.parent().find('.municipality-search').html('');
+	    //                } else {
+	    //                    yearEl.html('<option value="">Year</option>');
+	    //                    alert('"'+name+'" has no data, please choose another municipality');
+	    //                }
+	    //            }
+	    //        });
+	    //    },
+	    //    getCategories: function() {
+	    //        var data = {
+	    //            governmentsId:
+	    //                [
+	    //                    $('#first-municipality').attr('data-id'),
+	    //                    $('#second-municipality').attr('data-id')
+	    //                ]
+	    //        };
+	    //
+	    //        var el = $('#municipality-categories');
+	    //
+	    //        // get categories
+	    //        $.ajax({
+	    //            url: location.href,
+	    //            type: 'POST',
+	    //            data: data,
+	    //            success: function (data) {
+	    //                if (data.length > 0) {
+	    //                    el.html('<option value="">All categories</option>');
+	    //                    for (i=0;i<data.length;i++) {
+	    //                        el.append('<option value="'+data[i].id+'">'+data[i].caption+'</option>');
+	    //                    }
+	    //                } else {
+	    //                    el.html('<option value="">All categories</option>');
+	    //                }
+	    //            }
+	    //        });
+	    //    },
+	    //    submitForm: function($form) {
+	    //        // validation form
+	    //        var error = false;
+	    //        $('.municipality-compare').find('input, select').each(function() {
+	    //            if ($(this).val() == '' && $(this).attr('name') != 'municipality-compare[category]') {
+	    //                $(this).focus();
+	    //                error = true;
+	    //                return false;
+	    //            }
+	    //        });
+	    //
+	    //        if (error) {
+	    //            return false;
+	    //        }
+	    //
+	    //        var obj = this;
+	    //        var data = {
+	    //            comparedData: {
+	    //                firstMunicipality: {
+	    //                    id: $form.find('input[name="municipality-compare[first-municipality]"]').attr('data-id'),
+	    //                    name: $form.find('input[name="municipality-compare[first-municipality]"]').val(),
+	    //                    year: {
+	    //                        id: $form.find('select[name="municipality-compare[first-municipality-year]"]').val(),
+	    //                        name: $form.find('select[name="municipality-compare[first-municipality-year]"] option:selected').text()
+	    //                    },
+	    //                    data: {}
+	    //                },
+	    //                secondMunicipality: {
+	    //                    id: $form.find('input[name="municipality-compare[second-municipality]"]').attr('data-id'),
+	    //                    name: $form.find('input[name="municipality-compare[second-municipality]"]').val(),
+	    //                    year: {
+	    //                        id: $form.find('select[name="municipality-compare[second-municipality-year]"]').val(),
+	    //                        name: $form.find('select[name="municipality-compare[second-municipality-year]"] option:selected').text()
+	    //                    },
+	    //                    data: {}
+	    //                },
+	    //                category: {
+	    //                    id: $form.find('select[name="municipality-compare[category]"]').val(),
+	    //                    name: $form.find('select[name="municipality-compare[category]"] option:selected').text()
+	    //                }
+	    //            }
+	    //        }
+	    //
+	    //        data.comparedData.firstMunicipality['data'] = {};
+	    //        data.comparedData.secondMunicipality['data'] = {};
+	    //
+	    //        $.ajax({
+	    //            url: location.href,
+	    //            type: 'POST',
+	    //            data: data,
+	    //            success: function (comparedData) {
+	    //                if (comparedData.length > 0) {
+	    //                    for (i = 0; i < comparedData.length; i++) {
+	    //                        if (comparedData[i].governmentId == data.comparedData.firstMunicipality.id) {
+	    //                            data.comparedData.firstMunicipality['data'][comparedData[i].id] = comparedData[i];
+	    //                        }
+	    //                        if (comparedData[i].governmentId == data.comparedData.secondMunicipality.id) {
+	    //                            data.comparedData.secondMunicipality['data'][comparedData[i].id] = comparedData[i];
+	    //                        }
+	    //                    }
+	    //                }
+	    //                obj.data = data.comparedData;
+	    //
+	    //                console.log(obj.data);
+	    //                obj.drawDiagramm(obj.data.firstMunicipality.data, 'total-revenue-pie', 'Total Revenues');
+	    //                obj.drawDiagramm(obj.data.secondMunicipality.data, 'total-expenditures-pie', 'Total Expenditures');
+	    //            }
+	    //        });
+	    //    },
+	    //    drawDiagramm: function(data, blockId, title) {
+	    //        var obj = this;
+	    //        var chart, item, len3, options, p, r, ref1, rows, vis_data;
+	    //        vis_data = new google.visualization.DataTable();
+	    //        vis_data.addColumn('string', 'Total Gov. Expenditures');
+	    //        vis_data.addColumn('number', 'Total');
+	    //        rows = [];
+	    //
+	    //        var revenues = data;
+	    //        for(var key in revenues){
+	    //            if(revenues.hasOwnProperty(key) && (revenues[key].caption != title)) {
+	    //                r = [revenues[key].caption, parseInt(revenues[key].dollarAmount)];
+	    //                rows.push(r);
+	    //            }
+	    //        }
+	    //
+	    //        vis_data.addRows(rows);
+	    //        options = {
+	    //            'title': title,
+	    //            'titleTextStyle': {
+	    //                'fontSize': 16
+	    //            },
+	    //            'tooltip': {
+	    //                'textStyle': {
+	    //                    'fontSize': 12
+	    //                }
+	    //            },
+	    //            'width': 470,
+	    //            'height': 350,
+	    //            'pieStartAngle': 60,
+	    //            'sliceVisibilityThreshold': .05,
+	    //            'forceIFrame': true,
+	    //            'chartArea': {
+	    //                width: '90%',
+	    //                height: '75%'
+	    //            }
+	    //        };
+	    //        chart = new google.visualization.PieChart(document.getElementById(blockId));
+	    //        chart.draw(vis_data, options);
+	    //    }
+	    //};
+	    //
+	    //return compareGovernments;
 	}
 	
 	/**
@@ -475,6 +695,7 @@ webpackJsonp([1],[
 	    };
 	    chart = new google.visualization.PieChart(document.getElementById('total-revenue-pie'));
 	    chart.draw(vis_data, options);
+	
 	}
 	
 	/**
@@ -519,6 +740,7 @@ webpackJsonp([1],[
 	    };
 	    chart = new google.visualization.PieChart(document.getElementById('total-expenditures-pie'));
 	    chart.draw(vis_data, options);
+	
 	}
 	
 	
@@ -701,28 +923,30 @@ webpackJsonp([1],[
 	            }
 	        }
 	
-	        /**
-	         * Hide chart group. Group may contain few charts
-	         */
-	        function hideChartGroup(chartGroup, hide) {
 	
-	            var display = hide ? {display: 'none'} : {display: 'block'};
-	
-	            if (chartGroup == 'pie-charts') {
-	                $('#total-expenditures-pie').css(display);
-	                $('#total-revenue-pie').css(display);
-	
-	            } else if (chartGroup == 'tree-chart') {
-	                $('#total-tree').css(display);
-	
-	            } else if (chartGroup == 'tree-charts') {
-	                $('#total-expenditures-tree').css(display);
-	                $('#total-revenue-tree').css(display);
-	            }
-	
-	        }
 	
 	    });
+	}
+	
+	/**
+	 * Hide chart group. Group may contain few charts
+	 */
+	function hideChartGroup(chartGroup, hide) {
+	
+	    var display = hide ? {display: 'none'} : {display: 'block'};
+	
+	    if (chartGroup == 'pie-charts') {
+	        $('#total-expenditures-pie').css(display);
+	        $('#total-revenue-pie').css(display);
+	
+	    } else if (chartGroup == 'tree-chart') {
+	        $('#total-tree').css(display);
+	
+	    } else if (chartGroup == 'tree-charts') {
+	        $('#total-expenditures-tree').css(display);
+	        $('#total-revenue-tree').css(display);
+	    }
+	
 	}
 	
 	module.exports = {
@@ -777,15 +1001,22 @@ webpackJsonp([1],[
 	        template: '<div class="popover rankPopover" role="tooltip"><div class="arrow"></div><div class="popover-title-custom"><h3 class="popover-title"></h3></div><div class="popover-content"></div></div>'
 	    });
 	
-	    $(document).on('click', function(e) {
+	    $governmentController.on('click', function(e) {
+	        // Close other popovers
+	        if (!$(e.target).closest('.popover')[0]) {
+	            $('.rank').not(e.target).popover('destroy');
+	        }
+	    });
+	
+	    $statistics.on('click', function(e) {
+	        e.preventDefault();
+	        e.stopPropagation();
 	
 	        $element = $(e.target);
 	
 	        // Close other popovers
-	        if (/\brank\b/.test(e.target.className)) {
+	        if (!$(e.target).closest('.popover')[0]) {
 	            $('.rank').not(e.target).popover('destroy');
-	        } else {
-	            $('.rank').popover('destroy');
 	        }
 	
 	        $popover = $element.hasClass('rank') ? $element : $element.closest('.rank');
