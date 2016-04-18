b29063f90d8c1bc9bda73b7edcefb62b18af3990
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-04-18 15:23:39 +0600

    fix "change locale" url

--- src/GovWiki/FrontendBundle/Resources/public/js/common.js
+++ src/GovWiki/FrontendBundle/Resources/public/js/common.js
@@ -1,6 +1,13 @@
-window.changeLocale = require('./localization/localization.js');
-$loaderWrap = $('.loader_wrap');
-$loaderWrap.css({"opacity":0});
-window.setTimeout(function() {
-    $loaderWrap.css({"visibility":"hidden"});
-}, 1000);
+$(function() {
+    $loaderWrap = $('.loader_wrap');
+    $loaderWrap.css({"opacity":0});
+    window.setTimeout(function() {
+        $loaderWrap.css({"visibility":"hidden"});
+    }, 1000);
+
+    $('.locale-change').click(function (event) {
+        var loader_wrap = $('.loader_wrap');
+        loader_wrap.css({'opacity': 1, 'visibility': 'visible'});
+
+    })
+});
