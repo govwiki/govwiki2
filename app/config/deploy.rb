c4839405e279c6b24d96771a136aa1c3090a862d
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-05-13 23:13:19 +0600

    fix deploy config

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -25,9 +25,9 @@ set :git_shallow_clone, 1
 set :branch,            fetch(:branch, '').sub!(/^.*\//, "") # remove origin/ prefix from branch
 
 # Directories
-set :writable_dirs,         [app_path + "/cache", app_path + "/logs", web_path + "/documents", web_path + "/img/government/", web_path + "/img/upload"]
+set :writable_dirs,         [app_path + "/cache", app_path + "/logs", web_path + "/documents", web_path + "/img/government", web_path + "/img/upload"]
 set :shared_files,          ["app/config/parameters.yml"]
-set :shared_children,       [app_path + "/logs", web_path + "/img", web_path + "/img/government/", web_path + "/img/upload", web_path + "/documents"]
+set :shared_children,       [app_path + "/logs", web_path + "/img", web_path + "/img/government", web_path + "/img/upload", web_path + "/documents"]
 set :remote_backup_dir,     "/tmp"
 
 # Permissions
