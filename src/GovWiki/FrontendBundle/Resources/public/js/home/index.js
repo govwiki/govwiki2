40507332c0b6161c7d0858b91cd6f55a9515fb97
Dmitriy Shemin <dmitriy.shemin@sibers.com>
2016-05-13 19:43:47 +0600

    build js

--- src/GovWiki/FrontendBundle/Resources/public/js/home/index.js
+++ src/GovWiki/FrontendBundle/Resources/public/js/home/index.js
@@ -4,7 +4,7 @@ require('./map/main.js');
 
 $('#year-selector').change(function change() {
   $('#map').hide();
-  gw.map.year = $(this).find('option:selected').text();
+  gw.map.year = $(this).find('option:selected').text(); // eslint-disable-line
   sublayer.removeAllSubLayers();
   sublayer.reInit();
 });
